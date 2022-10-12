488065cc2b98c059465cf1dc26c6cfd5cf448bd6
Liviu Ciuca <lc874@canterbury.ac.uk>
2022-10-12 12:33:46 +0000

    generated the appropiate angular building blocks

--- angular_items_test/src/app/app.module.ts
+++ angular_items_test/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemsModule } from './items/items.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ItemsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
